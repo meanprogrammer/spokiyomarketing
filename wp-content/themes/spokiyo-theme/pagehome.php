2219f6c52a5be705a3552c3ce136181a88fed397
meanprogrammer <valiantdudz@yahoo.com>
2014-03-05 21:39:06 +0800

    popup contacts

--- wp-content/themes/spokiyo-theme/pagehome.php
+++ wp-content/themes/spokiyo-theme/pagehome.php
@@ -33,9 +33,9 @@ get_header(); ?>
             <div class="home-button-container">
 		            <h3>Looking for</h3>
 		           <!-- <div class="btn-group"> --> 
-					  <a class="btn btn-default btn-lg">Consultants</a>
-					  <a class="btn btn-default btn-lg">BPO Partner</a>
-					  <a class="btn btn-default btn-lg">Consulting Opportunity</a>
+					  <a class="btn btn-default btn-lg" id="consultantslink">Consultants</a>
+					  <a class="btn btn-default btn-lg" id="bpopartnerlink">BPO Partner</a>
+					  <a class="btn btn-default btn-lg" id="consultingopplink">Consulting Opportunity</a>
 				<!-- 	</div> -->
 				<a class="btn btn-primary btn-lg" data-toggle="modal" data-target="#signupModal">Sign Up For Newsletter</a>
 			</div>
@@ -44,6 +44,59 @@ get_header(); ?>
             </div>
         </div><!--end of banner-->
 
+<div class="modal fade" id="consultantsModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title">Looking For Consultants</h4>
+      </div>
+      <div class="modal-body">
+        <?php echo do_shortcode("[contact-form-7 id=\"55\" title=\"Consulting-Needs-Form\"]")?>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<div class="modal fade" id="bpoPartnerModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title">Looking For BPO Partner</h4>
+      </div>
+      <div class="modal-body">
+        <?php echo do_shortcode("[contact-form-7 id=\"58\" title=\"BPO-Partner-Form\"]")?>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<div class="modal fade" id="consultingOpportunityModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title">Looking For Consulting Opportunity</h4>
+      </div>
+      <div class="modal-body">
+        <?php echo do_shortcode("[contact-form-7 id=\"60\" title=\"Consulting-Opportunity-Form\"]")?>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
 		
 		<div class="modal prevent-vscroll" id="videoModal">
           <div class="modal-dialog" style="padding-top:80px;">
@@ -55,6 +108,50 @@ get_header(); ?>
         </div>
      </div><!-- #content -->
 	</div><!-- #primary -->
+<<script type="text/javascript">
+<!--
+	function hello(){
+		alert("hello dude");
+	}
 
+	$(document).ready(function(){
+		$('#consultingopplink').click(function() {
+			$('html').css('overflow-y', 'hidden');
+			$('html').css('margin-right', '15px');
+			$('#consultingOpportunityModal').modal({keyboard:false});
+		});
+
+		$('#consultingOpportunityModal').on('hidden.bs.modal', function (e) {
+			$('html').css('margin-right', '0');
+			$('html').css('overflow-y', 'auto');
+			
+		});
+
+		$('#consultantslink').click(function(){
+			$('html').css('overflow-y', 'hidden');
+			$('html').css('margin-right', '15px');
+			$('#consultantsModal').modal({keyboard:false});
+		});
+
+		$('#consultantsModal').on('hidden.bs.modal', function (e) {
+			$('html').css('margin-right', '0');
+			$('html').css('overflow-y', 'auto');
+			
+		});
+
+		$('#bpopartnerlink').click(function(){
+			$('html').css('overflow-y', 'hidden');
+			$('html').css('margin-right', '15px');
+			$('#bpoPartnerModal').modal({keyboard:false});
+		});
+
+		$('#bpoPartnerModal').on('hidden.bs.modal', function (e) {
+			$('html').css('margin-right', '0');
+			$('html').css('overflow-y', 'auto');
+			
+		});
+	});
+//-->
+</script>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
